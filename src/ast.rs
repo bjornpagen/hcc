18ef8c6b2ef54e1286b7d5b2544f6c7d64ac360d
Bjorn Pagen <bjornpagen@gmail.com>
2018-05-26 21:02:14 -0400

    add more garbage to ast

--- src/ast.rs
+++ src/ast.rs
@@ -1,28 +1,60 @@
 // hcc ast grammars
 
-pub enum ASTNode {
-    Decl(Prototype),
-    Stmt(Function),
-    Type(Datatype)
+// Decl is any static declaration (functions, classes, etc...)
+#[derive(PartialEq, Clone, Debug)]
+pub enum Decl {
+    Class(ClassDecl),
+    Def(DefDecl),
+    Func(FuncDecl),
+    Import(ImportDecl),
+    Null(NullDecl)
+}
+
+// Stmt is anything that *does* something
+#[derive(PartialEq, Clone, Debug)]
+pub enum Stmt {
+    Asm(AsmStmt),
+    Break(BreakStmt),
+    Compound(CompoundStmt),
+    Expr(Expr),
+    For(ForStmt),
+    FuncCall(FuncCall),
+    If(IfStmt),
+    Ret(RetStmt),
+    SwitchCase(SwitchCase),
+    Switch(SwitchStmt),
+    While(WhileStmt),
+    Value(ValueStmt)
 }
 
 #[derive(PartialEq, Clone, Debug)]
-pub struct Prototype {
+pub enum Expr {
+    BinaryOperator(BinaryOperator)
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub struct ClassDecl {
+    pub name: String,
+    pub access: Access,
+    pub vars: Vec(ValueStmt),
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum ValueStmt {
+    UnInit
+    Init
+    Union(Union
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub struct FuncDecl {
     pub name: String,
     pub args: Vec<String>
 }
 
 #[derive(PartialEq, Clone, Debug)]
-pub struct Function {
-    pub prototype: Prototype,
-    pub body: Expression
+pub enum Access {
+    Public,
+    Protected,
+    Private
 }
-
-#[derive(PartialEq, Clone, Debug)]
-pub enum Expression {
-    LiteralExpr(f64),
-    VariableExpr(String),
-    BinaryExpr(String, Box<Expression>, Box<Expression>),
-    CallExpr(String, Vec<Expression>)
-}
-
